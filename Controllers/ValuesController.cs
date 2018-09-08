a2fc56043d9bfbec4d37509c0a56370cb8bb67ae
emandere <emandere@gmail.com>
2018-09-08 06:50:51 -0400

    Second Commit Changed return string

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace TodoApi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Ed1", "Ed2" };
         }
 
         // GET api/values/5
