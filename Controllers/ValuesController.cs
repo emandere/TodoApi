f8acb27d0cf7a93dfae9a63fa7404e451d2cf822
emandere <emandere@gmail.com>
2018-09-14 05:43:06 -0400

    Fixed collection

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -27,7 +27,7 @@ namespace TodoApi.Controllers
             _server = _client.GetServer();
             _db = _server.GetDatabase("sampledb");
             var pairs = new List<String>();
-            foreach(Person pair in _db.GetCollection<Person>("person").FindAll())
+            foreach(Person pair in _db.GetCollection<Person>("people").FindAll())
             {
                pairs.Add(pair.name); 
             } 
